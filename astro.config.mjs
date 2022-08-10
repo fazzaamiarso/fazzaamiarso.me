c5b3d4a8ee893f735385d4349ebe0df0405d4249
fazzaamiarso <fazzaamiarso@gmail.com>
2022-08-11 00:16:58 +0700

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), image(), mdx()]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          black: "#101011",
+          white: "#fffffd",
+          red: "#e16358",
+        },
+        secondary: {
+          text: "#666764",
+          brown: "#f9f4f0",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
