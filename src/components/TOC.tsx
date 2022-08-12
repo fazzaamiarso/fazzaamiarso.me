6bbfefc13c50bf1d3bf23760c16513529f1c1983
fazzaamiarso <fazzaamiarso@gmail.com>
2022-08-12 21:36:56 +0700

    Table of content scroll spy working, but still need adjusment

--- src/components/TOC.tsx
+++ src/components/TOC.tsx
@@ -1,14 +1,56 @@
+import clsx from "clsx";
+import { useEffect, useRef, useState } from "react";
+
 type Props = {
   headings: { depth: number; text: string; slug: string }[];
 };
 
 export default function TOC({ headings }: Props) {
+  const tocRef = useRef<HTMLUListElement>(null);
+  const [currentId, setCurrentId] = useState("");
+
+  useEffect(() => {
+    if (!tocRef.current) return;
+
+    const setCurrent: IntersectionObserverCallback = (entries) => {
+      for (let entry of entries) {
+        if (entry.isIntersecting) {
+          const { id } = entry.target;
+          setCurrentId(id);
+          break;
+        }
+      }
+    };
+
+    const observerOption: IntersectionObserverInit = {
+      rootMargin: "0px 0px -66%",
+      threshold: 1,
+    };
+    const headingObserver = new IntersectionObserver(
+      setCurrent,
+      observerOption
+    );
+    document
+      .querySelectorAll("article :is(h2,h3,h4)")
+      .forEach((heading) => headingObserver.observe(heading));
+
+    return () => headingObserver.disconnect();
+  }, [tocRef.current]);
+
   return (
-    <ul className="space-y-2 mt-20">
+    <ul className="space-y-2 " ref={tocRef}>
       {headings.map((h) => {
         return (
-          <li key={h.slug} className="">
-            <a href={`#${h.slug}`} className="text-blue-400">
+          <li key={h.slug} className="text-ellipsis line-clamp-1">
+            <a
+              href={`#${h.slug}`}
+              className={clsx(
+                h.depth > 2 ? "pl-4" : "",
+                currentId.trim() === h.slug.trim()
+                  ? "text-primary-red"
+                  : "text-blue-400"
+              )}
+            >
               {h.text}
             </a>
           </li>
