9b32a50031f438ef82fec7d8cf953357bb4c647d
fazzaamiarso <fazzaamiarso@gmail.com>
2022-08-19 01:58:25 +0700

    Fix umami?

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,26 +7,30 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import { remarkReadingTime } from "./remark-plugin.mjs";
 import rehypeExternalLinks from "rehype-external-links";
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   // vite: { ssr: { external: ["svgo"] } },
-  integrations: [
-    react(),
-    mdx({
-      remarkPlugins: { extends: [remarkReadingTime] },
-      rehypePlugins: [
-        rehypeSlug,
-        rehypeAutolinkHeadings,
-        [rehypeExternalLinks, { rel: ["nofollow"], target: "_blank" }],
-      ],
-    }),
-    tailwind({ config: { applyBaseStyles: false } }),
-    image(),
-  ],
+  site: "https://fazzaamiarso.me",
+  integrations: [react(), mdx({
+    remarkPlugins: {
+      extends: [remarkReadingTime]
+    },
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, [rehypeExternalLinks, {
+      rel: ["nofollow"],
+      target: "_blank"
+    }]]
+  }), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image(), sitemap(), partytown()],
   markdown: {
     shikiConfig: {
-      theme: "material-ocean",
-    },
-  },
+      theme: "material-ocean"
+    }
+  }
 });
